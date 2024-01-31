5cf9a3f5c60322011cc00959316a3b441c9e3273
Davi Siqueira <davisvsiqueira@gmail.com>
2024-01-30 21:22:45 -0300

    feat: create initial transaction card style

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:expenses_app/models/transaction.dart';
 import 'package:flutter/material.dart';
+import "package:intl/intl.dart";
 
 void main() => runApp(const ExpensesApp());
 
@@ -7,12 +9,28 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomePage());
+    return MaterialApp(home: HomePage());
   }
 }
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  // just adding some placeholder objects to test the interface :)
+  final List<Transaction> _transactions = [
+    Transaction(
+      id: "id1",
+      title: "title1",
+      value: 123.56,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: "id2",
+      title: "title2",
+      value: 789.10,
+      date: DateTime.now(),
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -24,19 +42,64 @@ class HomePage extends StatelessWidget {
         ),
         backgroundColor: Colors.blue,
       ),
-      body: const Column(
+      body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          SizedBox(
+          const SizedBox(
             child: Card(
               elevation: 5,
               child: Text("Graph"),
             ),
           ),
-          Card(
-            child: Text("Transaction list"),
-          )
+          Column(
+              children: _transactions
+                  .map(
+                    (Transaction t) => Card(
+                      child: Row(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(
+                                color: Colors.purple,
+                                width: 2,
+                              ),
+                            ),
+                            padding: const EdgeInsets.all(10),
+                            margin: const EdgeInsets.symmetric(
+                                horizontal: 15, vertical: 10),
+                            child: Text(
+                              "\$${t.value.toStringAsFixed(2)}",
+                              style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                                color: Colors.purple,
+                              ),
+                            ),
+                          ),
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                t.title,
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                DateFormat.yMMMEd().format(t.date),
+                                style: TextStyle(
+                                  color: Colors.grey[600],
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                  .toList()),
         ],
       ),
     );
